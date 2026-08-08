0a4efb2273881f8917694db1190bb1c7b907929a
crownedxx <johnmarston1466@gmail.com>
2026-08-08 14:24:37 +1000

    oresupdated

--- src/lib/seedfinder-wasm.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.mjs" {
-  const factory: (opts: {
-    noInitialRun?: boolean;
-    print?: (s: string) => void;
-    printErr?: (s: string) => void;
-  }) => Promise<{ callMain: (args: string[]) => number }>;
-  export default factory;
-}
